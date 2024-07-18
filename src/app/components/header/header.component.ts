d0ed4061fb887909176e11b0c2218f4615722303
Ratneshsiddha <ratneshsiddha6@gmail.com>
2024-07-18 23:58:51 +0530

    navigating to shop page on search

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import { headerMenu } from '@app/faker/header-menu.faker';
+import { BaseComponent } from '@core/base/base.component';
 import { InputTextModule } from 'primeng/inputtext';
 @Component({
   selector: 'app-header',
@@ -12,11 +13,22 @@ import { InputTextModule } from 'primeng/inputtext';
   standalone: true
 })
 
-export class HeaderComponent {
+export class HeaderComponent extends BaseComponent {
   public fullText = "";
   public menu = headerMenu;
 
   constructor() {
+    super();
     console.log(this.menu)
+  }
+
+  search() {
+    const url = this.removeExtraSpaces(this.fullText).split(" ").join('-')
+    this.router.navigateByUrl(`/${url}?rawQuery=${this.fullText}`)
+  }
+  
+  private removeExtraSpaces(str: string): string {
+    // Replace multiple spaces with a single space
+    return str.replace(/\s+/g, ' ').trim();
   }
 }
--- src/app/core/base/base.component.ts
+++ src/app/core/base/base.component.ts
@@ -1,4 +1,8 @@
+import { inject } from "@angular/core";
+import { Router } from "@angular/router";
 import { AppRoutes } from "@shared/const/app.routes";
 export class BaseComponent {
+    public router = inject(Router);
+
     public appRoutes = AppRoutes
 }
