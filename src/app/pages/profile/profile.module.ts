be162e653992cb9c7f90c1538ca9023efc50b559
Ratneshsiddha <ratneshsiddha6@gmail.com>
2024-09-01 23:15:39 +0530

    created account details component

--- src/app/pages/profile/profile.module.ts
+++ src/app/pages/profile/profile.module.ts
@@ -4,12 +4,14 @@ import { CommonModule } from '@angular/common';
 import { ProfileRoutingModule } from './profile-routing.module';
 import { ProfileComponent } from './profile.component';
 import { OrdersComponent } from './components/orders/orders.component';
+import { AccountDetailsComponent } from './components/account-details/account-details.component';
 
 
 @NgModule({
   declarations: [
     ProfileComponent,
-    OrdersComponent
+    OrdersComponent,
+    AccountDetailsComponent
   ],
   imports: [
     CommonModule,
