2136ba70aa100bed03fdc6247fc6b69c52020005
Ratneshsiddha <ratneshsiddha6@gmail.com>
2024-09-04 23:39:15 +0530

    create change password component

--- src/app/pages/profile/profile.module.ts
+++ src/app/pages/profile/profile.module.ts
@@ -5,13 +5,15 @@ import { ProfileRoutingModule } from './profile-routing.module';
 import { ProfileComponent } from './profile.component';
 import { OrdersComponent } from './components/orders/orders.component';
 import { AccountDetailsComponent } from './components/account-details/account-details.component';
+import { ChangePasswordComponent } from './components/change-password/change-password.component';
 
 
 @NgModule({
   declarations: [
     ProfileComponent,
     OrdersComponent,
-    AccountDetailsComponent
+    AccountDetailsComponent,
+    ChangePasswordComponent
   ],
   imports: [
     CommonModule,
