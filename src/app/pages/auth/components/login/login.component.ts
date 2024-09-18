0d07722a108c56b6f1347fdbcd3bb94c4d52da6b
Ratneshsiddha <ratneshsiddha6@gmail.com>
2024-09-18 23:21:40 +0530

    designed opt block

--- src/app/pages/auth/components/login/login.component.ts
+++ src/app/pages/auth/components/login/login.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
+import { BaseComponent } from '@core/base/base.component';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
-export class LoginComponent {
-  value = "";
+export class LoginComponent extends BaseComponent {
+  mobileNumber = "";
+  enteredOTP = "";
+  showOtp: boolean = false
+
+  public login(){
+    this.showOtp = true;
+  }
+
+  public validateOtp(){
+    console.log(this.enteredOTP);
+    this.router.navigateByUrl(this.appRoutes.home);
+  }
 }
